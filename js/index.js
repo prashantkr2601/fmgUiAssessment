4906f4205bdce0107fc59b7781b00ce966b8359f
prashantkr2601 <prashantkr2601@gmail.com>
2023-08-22 06:40:41 +0530

    added functinality

---
+++ js/index.js
@@ -0,0 +1,569 @@
+const jobTopics = [
+  "Engineering",
+  "Pilots",
+  "Captains",
+  "First Officer",
+  "Cabin Crew",
+  "Office",
+  "Mechanic",
+  "Instructors",
+  "Technician",
+  "Computer Science",
+];
+
+const cardImgeData = [
+  "./assets/company-logo1.jpg",
+  "./assets/company-logo2.jpg",
+  "./assets/company-logo3.jpg",
+  "./assets/company-logo4.jpg",
+  "./assets/company-logo5.jpg",
+  "./assets/company-logo1.jpg",
+  "./assets/company-logo6.jpg",
+  "./assets/company-logo2.jpg",
+  "./assets/company-logo3.jpg",
+  "./assets/company-logo4.jpg",
+  "./assets/company-logo5.jpg",
+  "./assets/company-logo1.jpg",
+];
+
+const jobCategories = [
+  "A & P Mechanic",
+  "Aerospace",
+  "Analyst Jobs",
+  "Pinter & paintspayers Jobs",
+  "Office Jobs",
+  "Project Manager Jobs",
+  "Sales Job",
+  "Engneering Jobs",
+  "Fitter Jobs",
+  "Buyers Jobs",
+  "Inspector Jobs",
+  "Pilot Jobs",
+  "Executive Positions Jobs",
+  "Designer Jobs",
+  "Controller Jobs",
+  "Electrician Jobs",
+  "Cabin Crew Jobs",
+  "Controller jobs",
+  "Ground Crew Jobs",
+];
+
+const featuresData = [
+  {
+    iconUrl: "./assets/career.jpeg",
+    title: "Career Advice",
+    description:
+      "Career tips including how to update your CV, salary guides and interview tips, helping you find your next job",
+    ctaText: "Read our advice",
+  },
+  {
+    iconUrl: "./assets/alert.jpeg",
+    title: "Alerts",
+    description:
+      "Get the latest jobs sent directly to your inbox daily with our email alerts",
+    ctaText: "Set up alerts",
+  },
+  {
+    iconUrl: "./assets/people.jpeg",
+    title: "Top aviation employers",
+    description:
+      "Browse our directory of employers and recruiter agencies who are searching for aviation professionals",
+    ctaText: "View company A-Z",
+  },
+];
+
+const footerSectionData = [
+  {
+    headingText: "Jobseekers",
+    subHeadingText: [
+      "Register & Upload CV",
+      "Browse Jobs by location",
+      "Newest Jobs",
+      "Blog",
+      "Help",
+    ],
+  },
+  {
+    headingText: "Recruiters",
+    subHeadingText: [
+      "Advertise",
+      "Login",
+      "Associations",
+      "Recruiteng? <span>Call us on 01772639605</span>",
+    ],
+  },
+  {
+    headingText: "Stay in touch",
+    subHeadingText: [
+      "Contact Details",
+      "Sitemap",
+      "GDPR",
+      "Privacy Policy",
+      "Terms & Conditions",
+    ],
+    iconElement: [
+      '<i class="bi bi-linkedin"></i>',
+      '<i class="bi bi-facebook"></i>',
+      '<i class="bi bi-twitter"></i>',
+    ],
+  },
+];
+
+const jobSearchData = [
+  {
+    id: 1,
+    title: "full stack developer",
+    region: "mumbai",
+    company: "google",
+    salary: "100000",
+    skills: ["java", "python", "c++"],
+    experience: "2 years",
+    type: "full time",
+    description:
+      "for full stack developer we need to have knowledge of java,python,c++ and also we need to have 2 years of experience in this field and also we need to have knowledge of full stack development",
+  },
+  {
+    id: 2,
+    title: "front end developer",
+    region: "chennai",
+    company: "microsoft",
+    salary: "100000",
+    skills: ["reactJs", "c", "c++"],
+    experience: "2 years",
+    type: "full time",
+    description:
+      "for front end developer we need to have knowledge of java,python,c++ and also we need to have 2 years of experience in this field and also we need to have knowledge of front end development",
+  },
+  {
+    id: 3,
+    title: "back end developer",
+    region: "punjab",
+    company: "amazon",
+    salary: "100000",
+    skills: ["java", "python", "c++", "nodeJs"],
+    experience: "3 years",
+    type: "onSite",
+    description:
+      "for back end developer we need to have knowledge of java,python,c++ and also we need to have 2 years of experience in this field and also we need to have knowledge of back end development",
+  },
+  {
+    id: 4,
+    title: "full stack developer",
+    region: "delhi",
+    company: "facebook",
+    salary: "10000",
+    skills: ["java", "python", "c++", "mongodb"],
+    experience: "1 years",
+    type: "part time",
+    description:
+      "for full stack developer we need to have knowledge of java,python,c++ and also we need to have 2 years of experience in this field and also we need to have knowledge of full stack development",
+  },
+  {
+    id: 5,
+    title: "front end developer",
+    region: "banglore",
+    company: "apple",
+    salary: "1000000",
+    skills: ["java", "python", "c++", "reactJs", "angularJs", "vueJs"],
+    experience: "4 years",
+    type: "remote",
+    description:
+      "for front end developer we need to have knowledge of java,python,c++ and also we need to have 2 years of experience in this field and also we need to have knowledge of front end development",
+  },
+  {
+    id: 6,
+    title: "back end developer",
+    region: "mumbai",
+    company: "google",
+    salary: "100000",
+    skills: ["java", "python", "c++", "nodeJs", "mongodb"],
+    experience: "2 years",
+    type: "full time",
+    description:
+      "for back end developer we need to have knowledge of java,python,c++ and also we need to have 2 years of experience in this field and also we need to have knowledge of back end development",
+  },
+  {
+    id: 7,
+    title: "full stack developer",
+    region: "kolhapur",
+    company: "nutanix",
+    salary: "700000",
+    skills: ["nodeJs", "mongodb", "reactJs", "angularJs", "vueJs"],
+    experience: "5 years",
+    type: "onSite",
+    description:
+      "for full stack developer we need to have knowledge of java,python,c++ and also we need to have 2 years of experience in this field and also we need to have knowledge of full stack development",
+  },
+  {
+    id: 8,
+    title: "front end developer",
+    region: "noida",
+    company: "microsoft",
+    salary: "500000",
+    skills: ["java", "python", "c++", "reactJs", "angularJs", "vueJs"],
+    experience: "2 years",
+    type: "part time",
+    description:
+      "for front end developer we need to have knowledge of java,python,c++ and also we need to have 2 years of experience in this field and also we need to have knowledge of front end development",
+  },
+  {
+    id: 9,
+    title: "back end developer",
+    region: "pune",
+    company: "amazon",
+    salary: "1000000",
+    skills: ["java", "python", "c++", "nodeJs", "mongodb"],
+    experience: "3 years",
+    type: "remote",
+    description:
+      "for back end developer we need to have knowledge of java,python,c++ and also we need to have 2 years of experience in this field and also we need to have knowledge of back end development",
+  },
+  {
+    id: 10,
+    title: "full stack developer",
+    region: "india",
+    company: "facebook",
+    salary: "100000",
+    skills: ["java", "python", "c++", "nodeJs", "mongodb"],
+    experience: "2 years",
+    type: "full time",
+    description:
+      "for full stack developer we need to have knowledge of java,python,c++ and also we need to have 2 years of experience in this field and also we need to have knowledge of full stack development",
+  },
+  {
+    id: 11,
+    title: "front end developer",
+    region: "chennai",
+    company: "apple",
+    salary: "100000",
+    skills: ["java", "python", "c++", "reactJs", "angularJs", "vueJs"],
+    experience: "2 years",
+    type: "onSite",
+    description:
+      "for front end developer we need to have knowledge of java,python,c++ and also we need to have 2 years of experience in this field and also we need to have knowledge of front end development",
+  },
+  {
+    id: 12,
+    title: "back end developer",
+    region: "punjab",
+    company: "google",
+    salary: "100000",
+    skills: ["java", "python", "c++", "nodeJs", "mongodb"],
+    experience: "3 years",
+    type: "part time",
+    description:
+      "for back end developer we need to have knowledge of java,python,c++ and also we need to have 2 years of experience in this field and also we need to have knowledge of back end development",
+  },
+  {
+    id: 13,
+    title: "full stack developer",
+    region: "delhi",
+    company: "nutanix",
+    salary: "100560",
+    skills: ["java", "python", "c++", "nodeJs", "mongodb"],
+    experience: "1 years",
+    type: "remote",
+    description:
+      "for full stack developer we need to have knowledge of java,python,c++ and also we need to have 2 years of experience in this field and also we need to have knowledge of full stack development",
+  },
+  {
+    id: 14,
+    title: "front end developer",
+    region: "banglore",
+    company: "microsoft",
+    salary: "100000",
+    skills: ["java", "python", "c++", "reactJs", "angularJs", "vueJs"],
+    experience: "4 years",
+    type: "full time",
+    description:
+      "for front end developer we need to have knowledge of java,python,c++ and also we need to have 2 years of experience in this field and also we need to have knowledge of front end development",
+  },
+  {
+    id: 15,
+    title: "back end developer",
+    region: "banglore",
+    company: "FMG",
+    salary: "100000",
+    skills: ["java", "python", "c++", "reactJs", "angularJs", "vueJs"],
+    experience: "8 years",
+    type: "onSite",
+    description:
+      "for back end developer we need to have knowledge of java,python,c++ and also we need to have 2 years of experience in this field and also we need to have knowledge of back end development",
+  },
+  {
+    id: 16,
+    title: "full stack developer",
+    region: "banglore",
+    company: "friday media group",
+    salary: "126360",
+    skills: ["nodeJs", "mongodb", "reactJs", "angularJs", "vueJs"],
+    experience: "6 years",
+    type: "part time",
+    description:
+      "for full stack developer we need to have knowledge of java,python,c++ and also we need to have 2 years of experience in this field and also we need to have knowledge of full stack development",
+  },
+  {
+    id: 17,
+    title: "front end developer",
+    region: "banglore",
+    company: "friday media group",
+    salary: "100000",
+    skills: ["java", "python", "c++", "reactJs", "angularJs", "vueJs"],
+    experience: "2 years",
+    type: "hybrid",
+    description:
+      "for front end developer we need to have knowledge of java,python,c++ and also we need to have 2 years of experience in this field and also we need to have knowledge of front end development",
+  },
+  {
+    id: 18,
+    title: "back end developer",
+    region: "banglore",
+    company: "friday media group",
+    salary: "100000",
+    skills: ["java", "python", "c++", "nodeJs", "mongodb"],
+    experience: "3 years",
+    type: "remote",
+    description:
+      "for back end developer we need to have knowledge of java,python,c++ and also we need to have 2 years of experience in this field and also we need to have knowledge of back end development",
+  },
+  {
+    id: 19,
+    title: "full stack developer",
+    region: "banglore",
+    company: "friday media group",
+    salary: "100000",
+    skills: ["java", "python", "c++", "nodeJs", "mongodb"],
+    experience: "2 years",
+    type: "full time",
+    description:
+      "for full stack developer we need to have knowledge of java,python,c++ and also we need to have 2 years of experience in this field and also we need to have knowledge of full stack development",
+  },
+  {
+    id: 20,
+    title: "front end developer",
+    region: "banglore",
+    company: "friday media group",
+    salary: "100000",
+    skills: ["java", "python", "c++", "reactJs", "angularJs", "vueJs"],
+    experience: "2 years",
+    type: "onSite",
+    description:
+      "for front end developer we need to have knowledge of java,python,c++ and also we need to have 2 years of experience in this field and also we need to have knowledge of front end development",
+  },
+];
+
+const dropdownText = ["Dropdown 1", "Dropdown 2", "Dropdown 3"];
+
+function showJobTopics() {
+  let res = "";
+  jobTopics.map((item) => {
+    res += `<p role='button' class="bg-white mb-0 px-3 py-1 rounded fw-bold">${item}</p>`;
+  });
+  document.getElementById("showJobs").innerHTML = res;
+}
+
+function addDropdownText() {
+  let res = "";
+  dropdownText.map((item) => {
+    res += `<li><a class="dropdown-item" href="#">${item}</a></li>`;
+  });
+  document.getElementById("dropdown-menu").innerHTML = res;
+  document.getElementById("dropdown-menu-course").innerHTML = res;
+}
+
+function browseData() {
+  let res2 = "";
+  jobCategories.map((item) => {
+    res2 += `<p role='button' class="col">${item}</p>`;
+  });
+  document.getElementById("browseData").innerHTML = res2;
+}
+
+function featureCard() {
+  let res = "";
+  if (featuresData.length > 0) {
+    featuresData.forEach((item) => {
+      res += `<div class="card p-3 shadow-sm feature-card">
+          <div><img class="h-auto" width="80" height="80" src="${item.iconUrl}"></div>
+          <div class="card-body">
+            <h4 class="card-title fw-bold">${item.title}</h4>
+            <p class="card-text text-dark-emphasis overflow-hidden">${item.description}</p>
+            <a href="#" class="text-decoration-none">${item.ctaText}</a>
+          </div>
+          </div>`;
+    });
+    document.getElementById("featureCards").innerHTML = res;
+  }
+}
+
+function showFooter() {
+  const footer = document.getElementById("footer-section");
+
+  footerSectionData.map((element) => {
+    const div = document.createElement("div");
+    div.classList.add("footer__item");
+    div.innerHTML = `
+      <div>
+        <h5 class="fw-bold mb-3">${element.headingText}</h5>
+      </div>
+      <div>
+          ${element.subHeadingText
+            .map((subHeadingEle) => {
+              return `<p class="fw-bold">${subHeadingEle}</p>`;
+            })
+            .join("")}
+      </div>
+      <div class="icon-group d-flex gap-3">
+            ${
+              element.iconElement
+                ? element.iconElement
+                    .map((icon) => {
+                      return `<div class="border rounded-circle py-1 px-2">${icon}</div>`;
+                    })
+                    .join("")
+                : ""
+            }
+      </div>
+      `;
+    footer.appendChild(div);
+  });
+}
+
+var userAgent =
+  navigator.userAgent || navigator.vendor || window.opera || undefined;
+
+function isMobile() {
+  const regexs = [
+    /(Android)(.+)(Mobile)/i,
+    /BlackBerry/i,
+    /iPhone|iPod/i,
+    /Opera Mini/i,
+    /IEMobile/i,
+  ];
+  return regexs.some((b) => this.userAgent.match(b));
+}
+const isMobileCheck = isMobile();
+
+const noOfCardInRow = !isMobileCheck ? 6 : 1;
+
+function ArrayGroupsOfN(arrayToGroup, n) {
+  var groupsOfN = (total, item, index, array) => {
+    if (index % n === 0) {
+      total.push(array.slice(index, index + n));
+    }
+    return total;
+  };
+  return (arrayToGroup || []).reduce(groupsOfN, []);
+}
+
+const cardGroupsData = ArrayGroupsOfN(cardImgeData, noOfCardInRow);
+
+const carouselItem = cardGroupsData.map(
+  (cardGroupData, i) =>
+    `<div class="row-cols-1 row-cols-lg-6 carousel-item ${
+      i == 0 ? "active" : ""
+    }">
+  ${cardGroupData.map(
+    (item, j) =>
+      `<img src=${item}  alt="Slide ${i + j}" width="100" height="140"/>`
+  )}            
+        </div>`
+);
+window.onload = () => {
+  const carousel = document.getElementById("carousel-group");
+  carousel.innerHTML = carouselItem.join("").replaceAll(/\,/g, "");
+};
+
+window.addEventListener("load", () => {
+  showJobTopics();
+  addDropdownText();
+  browseData();
+  featureCard();
+  showFooter();
+});
+
+//Job Search Functionality
+
+function inputValidation(validatedRegex, inputValue, selectedId) {
+  if (
+    validatedRegex.test(inputValue) &&
+    inputValue !== "" &&
+    inputValue.trim().length > 1 &&
+    inputValue.trim().length < 30 &&
+    inputValue !== null &&
+    inputValue !== undefined &&
+    inputValue
+  ) {
+    document.getElementById(selectedId).style.border = "5px solid green";
+    return true;
+  } else {
+    document.getElementById(selectedId).style.border = "5px solid red";
+    return false;
+  }
+}
+
+function searchBoxValidation(inputValue, selectedId) {
+  let titleRegex = /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/g;
+  return inputValidation(titleRegex, inputValue, selectedId);
+}
+
+function handleJobSearch(event) {
+  event.preventDefault();
+
+  const title = document.getElementById("title").value;
+  const region = document.getElementById("region").value;
+
+  const validatedTitle = searchBoxValidation(title, "title");
+  const validatedRegion = searchBoxValidation(region, "region");
+
+  if (validatedTitle && validatedRegion) {
+    let filteredData = jobSearchData.filter(
+      (item) =>
+        item.title.toLowerCase().includes(title.toLowerCase()) &&
+        item.region.toLowerCase().includes(region.toLowerCase())
+    );
+
+    if (filteredData.length > 0) {
+      let output = "";
+      filteredData.forEach((item, i) => {
+        output += `
+        <div class="card mb-3">
+        <div class="card-body">
+          <h5 class="card-title">${item.title}</h5>
+          <h6 class="card-subtitle mb-2 text-muted">${item.company}</h6>
+          <img src=${
+            cardImgeData[i]
+          }  alt="Slide ${i}" width="100" height="140"/>
+          <p class="card-text">${item.description}</p>
+          <p class="card-text"><small class="text-muted">${
+            item.region
+          }</small></p>
+          <p>${item.skills
+            .map((skill) => {
+              return `<span class="badge bg-primary">${skill}</span>`;
+            })
+            .join("")}</p> 
+
+          <p>${item.type}</p>
+          <p>${item.experience}</p>
+          <p>${item.salary}</p>
+          <p>Apply Now</p>
+
+        </div>
+        </div>
+
+        `;
+      });
+
+      document.getElementById("searchResult").innerHTML = output;
+    } else {
+      document.getElementById(
+        "searchResult"
+      ).innerHTML = `<h3>No Result Found</h3>`;
+    }
+  }
+}
+
+document
+  .getElementById("searchForm")
+  .addEventListener("submit", handleJobSearch);
